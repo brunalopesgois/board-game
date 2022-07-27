5f2ea47fadd11080fde94b38efa4866aed28ed2a
Bruna Gois <bruna.gois@bitcapital.com.br>
2022-07-27 10:28:41 -0300

    feat: implement logs

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { GameModule } from './modules/game.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(GameModule);
+  const app = await NestFactory.create(GameModule, {
+    logger: console,
+  });
   await app.listen(8080);
 }
 bootstrap();
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 
 import { GameBoard } from '../entities/game-board.entity';
@@ -9,10 +9,13 @@ import { BehaviorType } from '../enums/behavior-type.enum';
 
 @Injectable()
 export class GameService {
-  //TODO: fazer logs eficientes para validar o comportamento
+  private readonly logger = new Logger(GameService.name);
+
   public simulate(): ResultInterface {
     const gameBoard: GameBoard = new GameBoard();
     gameBoard.properties = this.defineProperties();
+
+    this.logger.debug('Gameboard properties:', gameBoard);
 
     const cautiousPlayer = new Player({
       balance: 300,
@@ -77,8 +80,10 @@ export class GameService {
     const busteds: string[] = [];
     let continueGame = true;
 
+    this.logger.debug('Players order:', orderedPlayers);
+
     while (continueGame) {
-      for (const player of orderedPlayers) {
+      for (let player of orderedPlayers) {
         const { properties } = gameBoard;
 
         if (!player.busted) {
@@ -86,17 +91,18 @@ export class GameService {
             min: 1,
             max: 6,
           });
+
+          this.logger.debug('Dice:', moveForward);
+
           player.position =
             player.position + moveForward < 20
               ? player.position + moveForward
               : 0 + moveForward;
+
+          this.logger.debug(`Player ${player.behavior} moved to position: `, player.position);
+
           if (properties[player.position].owner !== null) {
-            if (player.balance - properties[player.position].rent < 0) {
-              player.busted = true;
-              busteds.push(player.behavior);
-            } else {
-              player.balance -= properties[player.position].rent;
-            }
+            player = this.payRent(player, properties[player.position], busteds);
           }
           if (properties[player.position].owner === null) {
             properties[player.position].owner = this.willBuyProperty(
@@ -111,6 +117,8 @@ export class GameService {
             continueGame = false;
           }
         }
+
+        gameBoard.properties = properties;
       }
     }
 
@@ -129,6 +137,10 @@ export class GameService {
       (a, b) => b.balance - a.balance,
     );
 
+    this.logger.debug('Rounds played:', round);
+    this.logger.debug('Final snapshot gameboard:', JSON.stringify(gameBoard));
+    this.logger.debug('Final snapshot players:', orderedByBalance);
+
     const resultOrderPlayers = orderedByBalance.map(
       (player) => player.behavior,
     );
@@ -146,12 +158,14 @@ export class GameService {
 
     if (player.behavior == BehaviorType.IMPULSIVE) {
       player.balance -= property.salePrice;
+      this.logger.debug(`Player ${player.behavior} buyed property with value: ${property.salePrice}. Balance left:`, player.balance);
       return player;
     }
 
     if (player.behavior == BehaviorType.DEMANDING) {
       if (property.rent > 50) {
         player.balance -= property.salePrice;
+        this.logger.debug(`Player ${player.behavior} buyed property with value: ${property.salePrice}. Balance left:`, player.balance);
         return player;
       }
       return null;
@@ -161,6 +175,7 @@ export class GameService {
       const finalBudget = player.balance - property.salePrice;
       if (finalBudget >= 80) {
         player.balance -= property.salePrice;
+        this.logger.debug(`Player ${player.behavior} buyed property with value: ${property.salePrice}. Balance left:`, player.balance);
         return player;
       }
       return null;
@@ -169,9 +184,29 @@ export class GameService {
     if (player.behavior == BehaviorType.RANDOM) {
       if (Math.random() < 0.5) {
         player.balance -= property.salePrice;
+        this.logger.debug(`Player ${player.behavior} buyed property with value: ${property.salePrice}. Balance left:`, player.balance);
         return player;
       }
       return null;
     }
+  }
+
+  private payRent(player: Player, property: Property, busteds: string[]): Player {
+    if (player.balance - property.rent < 0) {
+      player.busted = true;
+      busteds.push(player.behavior);
+      this.logger.debug(
+        `Player ${player.behavior} can't pay rent. Rent value: ${property.rent}. Player balance: ${player.balance} Current status: busted`,
+        player.busted,
+      );
+    } else {
+      player.balance -= property.rent;
+      this.logger.debug(
+        `Player ${player.behavior} have to pay the rent for ${property.owner} property. Balance left`,
+        player.balance,
+      );
+    }
+
+    return player;
   }
 }
